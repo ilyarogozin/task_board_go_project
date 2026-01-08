6a8443cdb67eb2d1f3d91bfd5c3928c35ea98773
ilyarogozin <rogozin.il2399@gmail.com>
2026-01-08 21:03:14 +0400

    changed board proto

--- board_service/migrations/000001_init.up.sql
+++ board_service/migrations/000001_init.up.sql
@@ -23,10 +23,7 @@ CREATE TABLE tasks (
 
 CREATE TABLE outbox_events (
     id UUID PRIMARY KEY,
-    aggregate_type TEXT NOT NULL,
-    aggregate_id UUID NOT NULL,
     event_type TEXT NOT NULL,
     payload JSONB NOT NULL,
-    created_at TIMESTAMP DEFAULT now(),
     processed BOOLEAN DEFAULT FALSE
 );
---
+++ proto/go.mod
@@ -0,0 +1,3 @@
+module github.com/ilyarogozin/task_board_go_project/proto
+
+go 1.25.5
